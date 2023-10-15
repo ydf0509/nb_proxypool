dba3680d449f85dd930fcc08995c4a1116230979
ydf0509 <909686719@qq.com>
2023-10-15 15:12:16 +0800

    no message

--- nb_proxypool/my_request_client.py
+++ nb_proxypool/my_request_client.py
@@ -2,12 +2,13 @@ import json
 import random
 
 from boost_spider.http.request_client import RequestClient
-from nb_proxypool.proxy_pool_config import redis_client,PROXY_KEY_IN_REDIS_DEFAULT
+from nb_proxypool.proxy_pool_config import redis_client, PROXY_KEY_IN_REDIS_DEFAULT
+
 
 class MyRequestClient(RequestClient):
     def _request_with_free_proxy(self, method, url, verify=None, timeout=None, headers=None, cookies=None, **kwargs):
         """使用redis中的快代理池子,怎么从redis拿代理ip和requests怎么使用代理，用户自己写"""
-        res = redis_client.zrange(PROXY_KEY_IN_REDIS_DEFAULT,0,-1)
+        res = redis_client.zrange(PROXY_KEY_IN_REDIS_DEFAULT, 0, -1)
         if len(res) == 0:
             err_msg = f'request_with_free_proxy redis {PROXY_KEY_IN_REDIS_DEFAULT} 键中没有代理ip'
             self.logger.error(err_msg)
@@ -23,13 +24,10 @@ class MyRequestClient(RequestClient):
     PROXYNAME__REQUEST_METHED_MAP = {'noproxy': RequestClient._request_with_no_proxy,
                                      'abuyun': RequestClient._request_with_abuyun_proxy,
                                      'kuai': RequestClient._request_with_kuai_proxy,
-                                     'free':_request_with_free_proxy,
+                                     'free': _request_with_free_proxy,
                                      }  # 用户新增了方法后，在这里添加代理名字和请求方法的映射映射
-
-
-
 
 
 if __name__ == '__main__':
     MyRequestClient(proxy_name_list=[
-                                     MyRequestClient.PROXY_FREE,MyRequestClient.PROXY_NOPROXY]).get('https://www.kuaidaili.com/free/intr/1')
+        MyRequestClient.PROXY_FREE, MyRequestClient.PROXY_NOPROXY]).get('https://www.kuaidaili.com/free/intr/1')
--- nb_proxypool/run_get_and_check_proxy.py
+++ nb_proxypool/run_get_and_check_proxy.py
@@ -8,31 +8,24 @@ from proxy_check import check_one_new_proxy, check_one_exist_proxy, scan_exists_
 
 @boost('get_proxies_from_sites', broker_kind=BrokerEnum.REDIS, qps=0.5)
 def get_proxies_from_sites(site_proxy_kls_name: str, page, proxy_type=1, ):
-    get_proxy_getter_kls(site_proxy_kls_name)(page=page,proxy_type=proxy_type).get_proxies()
-
-
-def add_site_task(site_proxy_kls: type(BaseProxyFromSiteGetter), interval_seconds):
-    get_proxies_from_sites.push(site_proxy_kls_name=site_proxy_kls.class_name(), page=p)
-    funboost_aps_scheduler.add_push_job(get_proxies_from_sites, 'interval', seconds=interval_seconds,
-                                        kwargs={"site_proxy_kls_name": site_proxy_kls.class_name(), "page": p})
+    get_proxy_getter_kls(site_proxy_kls_name)(page=page, proxy_type=proxy_type).get_proxies()
 
 
 if __name__ == '__main__':
     funboost_aps_scheduler.start()
     for p in range(1, 5):
         for site_kls in [Kuaidaili, Xici, Ip89, Ihuan, Zdaye, Beesproxy]:
-            if site_kls.support_page:
-                add_site_task(site_kls, interval_seconds=60 * p)
-            else:
-                if p == 1:
-                    add_site_task(site_kls, interval_seconds=60)
+            if site_kls.support_page or (site_kls.support_page is False and p == 1):
+                # 页数越靠后的，定时运行间隔越大，因为页数靠后的很少更新
+                funboost_aps_scheduler.add_push_job(get_proxies_from_sites, 'interval', seconds=p * 60,
+                                                    kwargs={"site_proxy_kls_name": site_kls.class_name(),
+                                                            "page": p})
     for p in range(1, 5):
         for proxy_type in [1, 2]:
             funboost_aps_scheduler.add_push_job(get_proxies_from_sites, 'interval', seconds=60 * p,
                                                 kwargs={"site_proxy_kls_name": Kaixin.class_name(),
                                                         "page": p, "proxy_type": proxy_type})
 
-    scan_exists_proxy.push()
     funboost_aps_scheduler.add_push_job(scan_exists_proxy, 'interval', seconds=30, )
 
     get_proxies_from_sites.consume()
